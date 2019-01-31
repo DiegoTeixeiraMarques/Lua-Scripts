1c2954cf9916eb91017af6af1114ff961e3d260b
Diego Teixeira <diegoteixeira1996@gmail.com>
2019-01-31 21:48:44 -0200

    Removing unnecessary files

--- DocumentationTest/main.lua
+++
@@ -1,9 +0,0 @@
-local ball = display.newImage("ball.png")
-ball.x = display.contentCenterX
-ball.y = display.contentCenterY
-
-local physics = require("physics")
-physics.start()
-
-physics.addBody(ball, "dynamic", {radius = 100})
-
